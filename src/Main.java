b9e59022b7b605476ecc6972c199fde36a0d8c2f
Golubev <golubev.s.g.25@gmail.com>
2022-03-17 14:53:14 +0300

    .gitignore added

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ticketPrice = 54_575;
+        int rubPerMile = 20;
+
+        int miles = ticketPrice / rubPerMile;
+        System.out.println("За покупку билета вам начислено " + miles + " миль(и).");
+    }
+}
+
+
+
